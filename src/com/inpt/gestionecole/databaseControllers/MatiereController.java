5deba3b55ea18efd81c75383f9d4d44df48ce86b
Jaafar Ben <Jaafar Ben@192.168.106.1>
2021-01-26 19:39:49 +0100

    Methods implementation for 'Matiere', update db.

--- src/com/inpt/gestionecole/databaseControllers/MatiereController.java
+++ src/com/inpt/gestionecole/databaseControllers/MatiereController.java
@@ -1,8 +1,14 @@
 package com.inpt.gestionecole.databaseControllers;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.inpt.gestionecole.config.Connexion;
+import com.inpt.gestionecole.models.Matiere;
+
 
 public class MatiereController {
 	Connection conn;
@@ -12,6 +18,43 @@ public class MatiereController {
 	public MatiereController() {
 		conn = Connexion.getConnection();
 	}
-
+	public int add(Matiere m) {
+		int nb=Connexion.Maj("insert into Matiere values("+m.getID_MATIERE()+","+m.getNOM_MATIERE()+","+m.getSEMESTRE_MATIERE()+")");
+		return nb;
+	}
+	public List<Matiere> allMatiere() {
+		List<Matiere> matiere = new ArrayList<Matiere>();
+	    ResultSet rs = Connexion.select("select * from Matiere");
+        try {
+			while (rs.next()){
+				Matiere M= new Matiere(rs.getInt(1),rs.getString(2),rs.getString(3));
+				matiere.add(M);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return matiere;
+	}
+	
+	public Matiere findMatierebyid(int ID_MATIERE) {
+		Matiere M=null;
+		ResultSet rs = Connexion.select("select * from Matiere where ID_MATIERE="+ID_MATIERE);
+		try {
+			if(rs.next()){
+			M = new Matiere(rs.getInt(1),rs.getString(2),rs.getString(3));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return M;
+	}
+	public int deleteMatiere(int ID_MATIERE) {
+		int nb = Connexion.Maj("delete from Matiere where ID_MATIERE="+ID_MATIERE);
+		return nb;
+	}
+	public int updateMatiere(Matiere M) {
+		int nb = Connexion.Maj("UPDATE `Matiere` SET `NOM_MATIERE` ="+M.getNOM_MATIERE()+", `SEMESTRE_MATIERE` = "+M.getSEMESTRE_MATIERE()+" WHERE `Matiere`.`ID_MATIERE` ="+M.getID_MATIERE()+"");
+		return nb;
+	}
 	// here add the methods that you will need
 }
--- src/com/inpt/gestionecole/models/Matiere.java
+++ src/com/inpt/gestionecole/models/Matiere.java
@@ -3,6 +3,7 @@ package com.inpt.gestionecole.models;
 public class Matiere {
 	int ID_MATIERE;
 	private String NOM_MATIERE;
+	private String SEMESTRE_MATIERE;
 
 	public Matiere() {
 
@@ -10,10 +11,11 @@ public class Matiere {
 	public Matiere (int iD_MATIERE ) {
 		this.ID_MATIERE  = iD_MATIERE;
 	}
-	public Matiere(int iD_MATIERE, String nOM_MATIERE) {
+	public Matiere(int iD_MATIERE, String nOM_MATIERE, String sEMESTRE_MATIERE) {
 		super();
 		ID_MATIERE = iD_MATIERE;
 		NOM_MATIERE = nOM_MATIERE;
+		SEMESTRE_MATIERE = sEMESTRE_MATIERE;
 	}
 
 	public int getID_MATIERE() {
@@ -31,5 +33,13 @@ public class Matiere {
 	public void setNOM_MATIERE(String nOM_MATIERE) {
 		NOM_MATIERE = nOM_MATIERE;
 	}
+	public String getSEMESTRE_MATIERE() {
+		return SEMESTRE_MATIERE;
+	}
+
+	public void setSEMESTRE_MATIERE(String sEMESTRE_MATIERE) {
+		SEMESTRE_MATIERE = sEMESTRE_MATIERE;
+	}
+
 
 }
